f40debf6d33d0e67a71f2b241f1c570413e2e701
fireundubh <fireundubh@gmail.com>
2022-04-29 18:46:59 -0700

    Support Anniversary Edition via CommonLibSSE-NG-Prebuilt

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,66 @@
-﻿#include "Version.h"
-#include "Papyrus\Registration.h"
+﻿#include "Papyrus\Registration.h"
+#include "Version.h"
 
 
+namespace
+{
+	void InitializeLog()
+	{
+#ifndef NDEBUG
+		auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+#else
+		auto path = logger::log_directory();
+		if (!path) {
+			return;
+		}
+
+		*path /= fmt::format(FMT_STRING("{}.log"), PROJECT_NAME);
+		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+#endif
+
+		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+
+#ifndef NDEBUG
+		log->set_level(spdlog::level::trace);
+#else
+		log->set_level(spdlog::level::info);
+		log->flush_on(spdlog::level::warn);
+#endif
+
+		spdlog::set_default_logger(std::move(log));
+		spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+
+		logger::info(FMT_STRING("{} v{}"), PROJECT_NAME, MATH_VERSION_VERSTRING);
+	}
+}
+
+// This export is only loaded by SKSE for 1.6 and up.
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
+	SKSE::PluginVersionData v;
+
+	v.PluginName(PROJECT_NAME);
+	v.PluginVersion(MATH_VERSION_MAJOR);
+
+	v.UsesAddressLibrary(true);
+	v.CompatibleVersions({ SKSE::RUNTIME_LATEST, SKSE::RUNTIME_1_5_97 });
+
+	return v;
+}();
+
+// This export is only loaded by SKSE for 1.5 and below
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
-#ifndef NDEBUG
-	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-#else
-	auto path = logger::log_directory();
-	if (!path) {
-		return false;
-	}
-
-	*path /= fmt::format(FMT_STRING("{}.log"), PROJECT_NAME);
-	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-#endif
-
-	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-
-#ifndef NDEBUG
-	log->set_level(spdlog::level::trace);
-#else
-	log->set_level(spdlog::level::info);
-	log->flush_on(spdlog::level::warn);
-#endif
-
-	set_default_logger(std::move(log));
-	spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
-
-	logger::info(FMT_STRING("{} v{}"), PROJECT_NAME, MATH_VERSION_VERSTRING);
-
 	a_info->infoVersion = SKSE::PluginInfo::kVersion;
 	a_info->name = PROJECT_NAME;
 	a_info->version = MATH_VERSION_MAJOR;
 
-	if (a_skse->IsEditor())
-	{
+	if (a_skse->IsEditor()) {
 		logger::critical("Loaded in editor, marking as incompatible"sv);
 		return false;
 	}
 
 	const auto ver = a_skse->RuntimeVersion();
-	if (ver < SKSE::RUNTIME_1_5_39)
-	{
+	if (ver < SKSE::RUNTIME_1_5_97) {
 		logger::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
 		return false;
 	}
@@ -52,6 +70,8 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
+	InitializeLog();
+
 	Init(a_skse);
 
 	Papyrus::Register();
