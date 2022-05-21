35c4f52ba1a7e986d4fa1e592a7eb878ea16acad
fireundubh <fireundubh@gmail.com>
2022-05-20 19:37:36 -0700

    Updated CommonLibSSE-NG to 3.3.0

--- src/main.cpp
+++ src/main.cpp
@@ -33,19 +33,6 @@ namespace
 	}
 }
 
-// This export is only loaded by SKSE for 1.6 and up.
-extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
-	SKSE::PluginVersionData v;
-
-	v.PluginName(PROJECT_NAME);
-	v.PluginVersion(MATH_VERSION_MAJOR);
-	v.AuthorName("fireundubh");
-	v.UsesAddressLibrary(true);
-	v.CompatibleVersions({ SKSE::RUNTIME_LATEST, SKSE::RUNTIME_1_5_97 });
-
-	return v;
-}();
-
 // This export is only loaded by SKSE for 1.5 and below
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
@@ -59,13 +46,26 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
 	}
 
 	const auto ver = a_skse->RuntimeVersion();
-	if (ver < SKSE::RUNTIME_1_5_97) {
+	if (ver < SKSE::RUNTIME_SSE_1_5_97) {
 		logger::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
 		return false;
 	}
 
 	return true;
 }
+
+// This export is only loaded by SKSE for 1.6 and up.
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
+	SKSE::PluginVersionData v;
+
+	v.PluginName(PROJECT_NAME);
+	v.PluginVersion(MATH_VERSION_MAJOR);
+	v.AuthorName("fireundubh");
+	v.UsesAddressLibrary(true);
+	v.CompatibleVersions({ SKSE::RUNTIME_SSE_LATEST_AE, SKSE::RUNTIME_SSE_1_5_97 });
+
+	return v;
+}();
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
